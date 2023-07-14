573b7f6edfad44f1041f2e042f7e5a5e09633ff2
Metais Bastien <bastienmetais79@gmail.com>
2023-07-14 20:47:45 +0200

    final commit

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -5,6 +5,7 @@ import {onBeforeMount, onMounted, reactive, ref, watch} from "vue";
 import {GameMethods} from "@/methods/GameMethods";
 import {CardsMethod, initialCards} from "@/methods/CardsMethod";
 import {CardModel} from "@/models/CardModel";
+import Modal from "@/components/Modal.vue";
 
 const generatedCards = CardsMethod.generatePairs(initialCards);
 const shuffledCards = CardsMethod.shuffleCard(generatedCards);
@@ -51,7 +52,7 @@ function getCardShowedWithoutFindedOne() {
 
 function onCardClick(index: number) {
   currentGame.listCard[index].isShowed = !currentGame.listCard[index].isShowed
-  currentGame.numberPlay++;
+  if(getCardShowedWithoutFindedOne().length === 2) currentGame.numberPlay++
 }
 </script>
 
@@ -63,5 +64,20 @@ function onCardClick(index: number) {
         </div>
       </template>
     </div>
-  couycu
+  <div class="d-flex flex-column justify-content-between h-50">
+    <div>
+      <div v-if="currentGame.numberPairFinded !== initialCards.length" class="alert alert-primary mt-4 " role="alert">
+        Nombre de coups joués : {{ currentGame.numberPlay }}
+      </div>
+      <div v-if="currentGame.numberPairFinded !== initialCards.length" class="alert alert-secondary" role="alert">
+        Nombre de paires trouvées : {{ currentGame.numberPairFinded }}
+      </div>
+      <div v-if="currentGame.numberPairFinded == initialCards.length" class="alert alert-success mt-4" role="alert">
+        Bravo, vous avez trouvé toutes les paires en {{ currentGame.numberPlay }} coups !
+      </div>
+    </div>
+    <div class="mb-5 align-content-center">
+      <Modal></Modal>
+    </div>
+  </div>
 </template>
---
+++ src/components/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Rejouer
+  </button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Rejouer</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Vous êtes sur le point de relancer une partie, la partie en cours sera perdue.
+          Etes-vous sûr de vouloir continuer ?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+          <button type="button" class="btn btn-primary" @click="this.$router.go(this.$router.currentRoute)">Rejouer</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
